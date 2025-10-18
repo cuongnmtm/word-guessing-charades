32d20952a0205811e52f48a947daa88a6d6b9766
Cuong, Nguyen Minh Tran Manh <cuong.nmtm@komit-consulting.com>
2025-10-18 22:58:49 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/word-guessing-charades/sw.js', { scope: '/word-guessing-charades/' })})}
